e77d1f34e62c035e5897c3d9f98d4f9e601da587
Elias Moosman <elias@youiest.com>
2014-10-15 13:19:48 +0400

    login folder added

--- admin/server/adminServer.js
+++ admin/server/adminServer.js
@@ -1,17 +1,18 @@
-adminUser = ["nicolsondsouza","bgbhavesh","hastenf","wiber"];
-function isAdmin(user){
-	for(var i=0,il=adminUser.length;i<il;i++){
-		if(user.username == adminUser[i]){
-			return true;
-		}
-	}
-	return false;
+adminUser = ["nicolsondsouza", "bgbhavesh", "hastenf", "vviber"];
+
+function isAdmin(user) {
+    for (var i = 0, il = adminUser.length; i < il; i++) {
+        if (user.username == adminUser[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 Meteor.users.find({}).observe({
-	"added" : function(user){
-		if(isAdmin(user)){
-			Roles.addUsersToRoles(user._id, ['admin']);
-		}
-	}
+    "added": function(user) {
+        if (isAdmin(user)) {
+            Roles.addUsersToRoles(user._id, ['admin']);
+        }
+    }
 });
