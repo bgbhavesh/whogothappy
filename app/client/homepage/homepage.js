a857d1a1e95418b9ff25547b3eeda2424562352f
bgbhavesh <bgbhavesh@gmail.com>
2014-10-17 17:06:51 +0530

    added event on home page top bar

--- app/client/homepage/homepage.js
+++ app/client/homepage/homepage.js
@@ -4,6 +4,18 @@ Template.home.events({
 	// 	Session.set("placeId",this.place_id);
 	// 	Session.set("placeName",this.name);
 	// }
+    'click .move-left': function (e){
+        $(".move-left").removeClass('move-left')                    
+    },
+    'click .move-right': function (e){
+        $(".move-right").removeClass('move-right')                    
+    },
+    'click .left-off-canvas-toggle': function (e){
+        $(".off-canvas-wrap").addClass('move-right')                    
+    },
+    'click .right-off-canvas-toggle': function (e){
+       $(".off-canvas-wrap").addClass('move-left')      
+    },
     'click .daTe': function (e) {
         // $(".daTe").css("display","none")
         // $(".countDown").css("display","block")
