50aa1045e4605a2647f514964ffd1f7fd87fbdf1
nicolsondsouza <nicolsondsouza@gmail.com>
2014-10-21 18:18:39 +0530

    admin for facebook fixed

--- admin/server/adminServer.js
+++ admin/server/adminServer.js
@@ -1,10 +1,17 @@
 adminUser = ["nicolsondsouza", "bgbhavesh", "hastenf", "vviber"];
-
+adminUserFacebook = ["100000002030165","100000488108267","100000002030165","100000002030165"];
+//                      nicolsondsouza   bgbhavesh         hastenf            vViber
 function isAdmin(user) {
     var setTime = new Date().getTime();
     log("isAdmin" + setTime,1);
     for (var i = 0, il = adminUser.length; i < il; i++) {
         if (user.username == adminUser[i]) {
+            log("isAdmin" + (new Date().getTime() - setTime),1);
+            return true;
+        }
+    }
+    for (var i = 0, il = adminUserFacebook.length; i < il; i++) {
+        if (user._id == adminUserFacebook[i]) {
             log("isAdmin" + (new Date().getTime() - setTime),1);
             return true;
         }
