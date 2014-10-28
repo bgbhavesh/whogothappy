d174658ef88182b9308a70d79b71b1a108f814b0
nicolsondsouza <nicolsondsouza@gmail.com>
2014-10-28 20:57:46 +0530

    hash router added

--- route/client/routeClient.js
+++ route/client/routeClient.js
@@ -1,14 +1,4 @@
-Template.__define__("route", (function() {
-	var view = this;
-	var templateName = HashRouter.getRoute(Session.get("route"));
-	if(Template[templateName]){
-		console.log(Spacebars.include(view.lookupTemplate(templateName)))
-		return Spacebars.include(view.lookupTemplate(templateName));
-	}
-	else{
-		return Spacebars.include(view.lookupTemplate("home"));
-	}
-}));
+
 
 // Template.route.helpers({
 // 	"route" : function(first){
@@ -21,48 +11,7 @@ Template.__define__("route", (function() {
 // 			return true;
 // 	}
 // });
-HashRouter = {};
-HashRouter.go = function(hash){
-	window.location.hash = hash;
-}
-HashRouter.routes = {};
-HashRouter.add = function(options){
-	if(!options){
-		throw new Error("Please send json object with {'hash':}");
-	}
 
-	if(HashRouter.routes[options.route]){
-		throw new Error("Route already Exists " +options.route);
-	}
-	else{
-		HashRouter.routes[options.route] = options
-	}
-	// options.route;
-	// options.template;
-	// options.beforeFunction;
-	// options.afterFunction;
-}
-
-HashRouter.getRoute = function(routeName){
-	if(HashRouter.routes[routeName]){
-		return HashRouter.routes[routeName].template;
-	}
-	else{
-		console.log("Route Not found " +routeName);
-		return null;
-	}
-}
-HashRouter.onHashChange = function(){
-	var hash = window.location.hash;
-	Session.set("route",hash);
-	
-	// if(app.routerFunction[hash])
-	// 	app.routerFunction[hash]()
-	// else
-	// 	app.routerFunction["#home"]();
-}
-
-$(window).on('hashchange',HashRouter.onHashChange);
 
 HashRouter.add({"route" : "#login", "template" : "login"});
 HashRouter.add({"route" : "#admin", "template" : "admin"});
@@ -120,7 +69,7 @@ HashRouter.add({"route" : "#home", "template" : "home"});
 
 
 
-HashRouter.onHashChange();
+
 
 
 Router = {};
