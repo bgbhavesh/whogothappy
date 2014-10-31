630a4972e96370492eb9c4c0253ef798048c66da
nicolsondsouza <nicolsondsouza@gmail.com>
2014-10-31 20:18:48 +0530

    before pull

--- gdrive/gdriveCommon.js
+++ gdrive/gdriveCommon.js
@@ -1,11 +1,1 @@
 
-Meteor.startup(function(){
-	var my_sheet = new GoogleSpreadsheet('1Sn3TQLUaILVjp5KMy1cvDsfaH3ISUtkHAUg7VmMsAj8');
-
-	// my_sheet.getRows( 1, function(err, row_data){
-	// 	console.log(row_data);
- 	//	console.log( 'pulled in '+row_data.length + ' rows ')
-	// });
-
-})
-
