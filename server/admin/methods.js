6241eb4b7e76316fd978ee3d9bc3416ed99d4c57
hastenfernandes <hastenf@gmail.com>
2014-11-20 12:58:21 +0530

    push ready

--- server/admin/methods.js
+++ server/admin/methods.js
@@ -1,7 +1,7 @@
 Meteor.methods({
 	"getPushId" : function(options){
 		if(Meteor.userId())
-			Meteor.users.update({"_id":Meteor.userId()},{$set:{"profile.pushId":options.pushId,"profile.pushServer":options.pushServer}});
+			Meteor.users.update({"_id":Meteor.userId()},{$set:{"profile.pushId":options.pushId,"profile.pushServer":options.pushDevice}});
 			app.pushServer.sendAndroid("hello", [options.pushId], "whogothappy", "whogothappy", 1);
 	}
 });
