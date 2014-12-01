776a9dd7647e4c8de4454a1def245ee0ee179598
bgbhavesh <bgbhavesh@gmail.com>
2014-12-01 15:44:30 +0530

    weeks prepand

--- client/menu/weeks.js
+++ client/menu/weeks.js
@@ -13,24 +13,73 @@ app.arrangeDays = function(){
 		$("#weeks thead tr").prepend("<th>"+i+"</th>");	
 	};
 	$("#weeks thead tr").prepend("<th>"+app.lang.table.week+"</th>");
-
+/*************************header is done ************************************/
 	$("#weeks tbody .endgame1").html("");
-	$("#weeks tbofordy .endgame1").append("<th>"+app.lang.table.col1+"</th>");
-	// for (var i = n; i <= 6; i++) {
-	// 	console.log("d"+i);
+	for (var i = n; i <= 6; i++) {
+		var c, v;
+		c = "grey";
+		v = "o";
+		if(Streak.findOne({"day": i})){
+			if(Streak.findOne({"day": i}).endgame1){
+				c = "black";
+			}
+			if(Streak.findOne({"day": i}).first){
+				v = "x";
+			}
+		}
+		// console.log(Streak.findOne({"day": i}).endgame1);
+		$("#weeks tbody .endgame1").prepend('<td class="'+c+'">'+v+'</td>');	
+	};
+	for (var i = 0; i < n; i++) {
+		var c, v;
+		c = "grey";
+		v = "o";
+		if(Streak.findOne({"day": i})){
+			if(Streak.findOne({"day": i}).endgame1){
+				c = "black";
+			}
+			if(Streak.findOne({"day": i}).first){
+				v = "x";
+			}
+		}
+		// console.log(Streak.findOne({"day": i}).endgame1);
+		$("#weeks tbody .endgame1").prepend('<td class="'+c+'">'+v+'</td>');	
+	};
+	$("#weeks tbody .endgame1").prepend("<th>"+app.lang.table.col1+"</th>");
 
-	// 	$("#weeks tbofordy .endgame1").append('<td class="{{#if d'+i+'.endgame1}} black {{else}} grey {{/if}}">{{#if d'+i+'.first}} x {{else}} o {{/if}}</td>');	
-	// };
-	// for (var i = 0; i < n; i++) {
-	// 	$("#weeks tbofordy .endgame1").append('<td class="{{#if d'+i+'.endgame1}} black {{else}} grey {{/if}}">{{#if d'+i+'.first}} x {{else}} o {{/if}}</td>');	
-	// };
-
+/*************************AM is done ************************************/
 	$("#weeks tbody .endgame2").html("");
-	$("#weeks tbody .endgame2").append("<th>"+app.lang.table.col2+"</th>");
-	// for (var i = n; i <= 6; i++) {
+	for (var i = n; i <= 6; i++) {
+		var c, v;
+		c = "grey";
+		v = "o";
+		if(Streak.findOne({"day": i})){
+			if(Streak.findOne({"day": i}).endgame2){
+				c = "black";
+			}
+			if(Streak.findOne({"day": i}).second){
+				v = "x";
+			}
+		}
+		$("#weeks tbody .endgame2").prepend('<td class="'+c+'">'+v+'</td>');	
+		// console.log(Streak.findOne({"day": i}).endgame2);
 	// 	$("#weeks tbody .endgame2").append('<td class="{{#if d'+i+'.endgame2}} black {{else}} grey {{/if}}">{{#if d'+i+'.second}} x {{else}} o {{/if}}</td>');
-	// };
-	// for (var i = 0; i < n; i++) {
+	};
+	for (var i = 0; i < n; i++) {
+		var c, v;
+		c = "grey";
+		v = "o";
+		if(Streak.findOne({"day": i})){
+			if(Streak.findOne({"day": i}).endgame2){
+				c = "black";
+			}
+			if(Streak.findOne({"day": i}).second){
+				v = "x";
+			}
+		}
+		$("#weeks tbody .endgame2").prepend('<td class="'+c+'">'+v+'</td>');	
+		// console.log(Streak.findOne({"day": i}).endgame2);
 	// 	$("#weeks tbody .endgame2").append('<td class="{{#if d'+i+'.endgame2}} black {{else}} grey {{/if}}">{{#if d'+i+'.second}} x {{else}} o {{/if}}</td>');
-	// };
+	};
+	$("#weeks tbody .endgame2").prepend("<th>"+app.lang.table.col2+"</th>");
 } 
