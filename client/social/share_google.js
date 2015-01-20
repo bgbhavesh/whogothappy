cebcb281d0498640461a190a68fc6e046f83d3a1
Trinisoft Technologies <trinisofttechnologies>
2015-01-20 13:50:07 +0530

    some login issue

--- client/social/share_google.js
+++ client/social/share_google.js
@@ -1,5 +1,5 @@
 
-window.___gcfg = {
-	parsetags: 'onload'
-};
-$("head").append('<script src="https://apis.google.com/js/client:platform.js" async defer></script>');
+// window.___gcfg = {
+// 	parsetags: 'onload'
+// };
+// $("head").append('<script src="https://apis.google.com/js/client:platform.js" async defer></script>');
