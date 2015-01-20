79fa9b965b7b1913256874bebb36c2d5c0386d59
Trinisoft Technologies <trinisofttechnologies>
2015-01-20 17:39:30 +0530

    iron-router

--- client/route/routeClient.js
+++ client/route/routeClient.js
@@ -1,8 +1,8 @@
 Router.route('/', function () {
 	this.render('home', {
-		data: function () { 
-			// return Items.findOne({_id: this.params._id}); 
-		}
+		// data: function () { 
+		// 	// return Items.findOne({_id: this.params._id}); 
+		// }
 	});
 });
 
