d975cf534c4a7e39a457e7625751c163320c1582
trinisoft <trinisofttechnologies@gmail.com>
2015-05-01 12:06:40 +0530

    drag half is done

--- client/famous/localPopUp/local_popUp.js
+++ client/famous/localPopUp/local_popUp.js
@@ -7,6 +7,10 @@ Template.gamePopUp2.events({
         app.closeCounter2();
     },
 });
+app.closeOverlayImgRank = function(){
+    $(".rankImg").css("display","none");
+    
+}
 app.openOverlay2 = function(){
     $("#tapTapEnded2").css("display","block");
     $("#clickEvent2").css("-webkit-filter", "blur(4px)");
@@ -21,6 +25,40 @@ var hours =0;
 var mins =0;
 var seconds =0;
 var timex;
+var startDrag = false;
+Template.rankImg.events({
+    // 'click .overlay':function(event){
+    //     console.log(event.clientY)
+    // },
+    // 'mousedown .overlay .dragaAndImg':function(event){
+    //     console.log(event.clientY)
+    // },
+    // 'dragstart .overlay .dragaAndImg':function(event){
+    //     console.log(event)
+    // },
+    // 'dragend .overlay .dragaAndImg':function(event){
+    //     console.log(event)
+    // },
+    'drag .overlay .dragaAndImg':function(event){
+        startDrag = true;
+        console.log(event);
+        // console.log(event.currentTarget.offsetTop);
+        // console.log(event.currentTarget.offsetLeft);
+        // console.log(event.clientX);
+        // console.log(event.clientY);
+
+       $(".dragaAndImg").css({"top":event.currentTarget.offsetTop+"px","left":event.currentTarget.offsetLeft+"px"});
+    },
+    'mouseout .overlay .dragaAndImg':function(event){
+        if(startDrag){
+            console.log(event.clientY)
+            // $(".dragaAndImg").css({"top":event.clientX+"px","left":event.clientY+"px"});
+        }
+
+        startDrag = false;
+    },
+    
+})
 Template.content2.helpers({
     image : function(){
         Session.get("startGameFlag");
@@ -29,103 +67,120 @@ Template.content2.helpers({
     },
 })
 Template.content2.events({
-    "click #clickEvent2 img" : function(event){
-        if(app.clickStart == false)
-            return;
-        app.clickStart = false;
-        app.caseCount++;
+    "dblclick #clickEvent2 img":function(event){
         var element = event.currentTarget;
-        var $img = $(event.target);
-        var offset = $img.offset();
-        var xx = event.clientX - offset.left - 5;
-        var yy = event.offsetY-5;
-        var x = event.clientX - offset.left;
-        var y = event.offsetY;
-        x = Math.abs(parseInt(x, 10)) -50;
-        y = Math.abs(parseInt(y, 10)) -45;
-        x = Math.abs(x);
-        y = Math.abs(y);
-        $(element).parent().append('<div id="correctdotOnImg" style="position: absolute;top:45%;left:45%;height: 10px;width: 10px;background: green;opacity: 0.6; border-radius: 100%;"></div>');
-        $(element).parent().append('<div id="dotOnImg" style="position: absolute;top:'+yy+'px;left:'+xx+'px;height: 10px;width: 10px;background: red;opacity: 0.6; border-radius: 100%;"></div>');
-        app.AccuracyPoints = Math.floor( (x + y) / 10 );
-        smileDuration = app.randomNumber(parseInt(app.lang.settings.showSmileyMax),parseInt(app.lang.settings.showSmileyMin))
-        var str = $(event.currentTarget).attr("src");
-        var res = str.match("joy");
-        var joySrc = "";
-        var rotateFlag = Session.get("rotate");
-        if(Session.get("flip") && !rotateFlag){
-            var imgsUrl = $("#clickEvent2 div figure.back img");
-            for(var i=0,il=imgsUrl.length;i<il;i++){
-                var imgSrc = imgsUrl[i].getAttribute("src")
-                if(imgSrc.match("joy")){
-                    joySrc = imgsUrl[i].src;
-                    // console.log("if"+joySrc)
-                    imgsUrl[i].src = "./images/expression/smily.png";
-                    setTimeout(function(){
-                        app.changeFace(joySrc,res);
-                    },smileDuration);// correct into smile show s
-                }
-            }
-        }else{
-            var imgsUrl = $("#clickEvent2 div figure.front img");
-            // console.log(imgsUrl)
-            for(var i=0,il=imgsUrl.length;i<il;i++){
-                var imgSrc = imgsUrl[i].getAttribute("src")
-                if(imgSrc.match("joy")){
-                    joySrc = imgsUrl[i].src;
-                    // console.log("else"+joySrc)
-                    imgsUrl[i].src = "./images/expression/smily.png";
-                    setTimeout(function(){
-                        app.changeFace(joySrc,res);
-                    },smileDuration);// correct into smile
-                }
-            }
+        var SRC = event.currentTarget.currentSrc;
+        var size = event.currentTarget.clientHeight;
+        var max_height = $("#clickEvent2").width();
+        var leftscreenX = (event.clientX < max_height)?event.clientX:max_height-(max_height/8);
+        var leftscreenY = (event.clientY < max_height)?event.clientY:max_height-(max_height/8); 
+        if (element) {
+            $(".rankImg").css("display","block");
+            var style = 'style="left:'+leftscreenX+'px;top:'+leftscreenY+'px ;width:'+size/2+'px; height:'+size/2+';" '
+            var selection = '<div class="dragaAndImg" '+style+' dragable="false" ><img src="'+SRC+'" width="'+size/2+'" height="'+size/2+'"></div>';
+            $('.rankImg').append(selection)
 
-        }
-        endtime = new Date().getTime()
-        totalTime = endtime - app.slideStartTime;
-        var delay = app.randomNumber(parseInt(app.lang.settings.tranisionWaitMin),parseInt(app.lang.settings.tranisionWaitMax));
-        count--;
-        var late = parseInt(app.lang.settings.lateClick);
-        if(res){ 
-            if(app.score.method){
-                if(app.score.method.length!=0){
-                    if(totalTime<late){
-                        result = parseInt(app.lang.settings.sixteenScorePerHit - app.AccuracyPoints); 
-                    }else{
-                        result = parseInt(app.lang.settings.sixteenScorePerLateHit);
-                    }
-                }else{
-                        result = parseInt(app.lang.settings.sixteenScorePerHit - app.AccuracyPoints);
-                }
-            }
-            // delay = 2000;
-            app.totalscore = app.totalscore + result;
-        }else{
-            result = 0;
-        }
-        // console.log(app.AccuracyPoints)
-        app.score.method.push({
-            "slideStartTime": app.slideStartTime,
-            "endtime": endtime,
-            "totalTime": totalTime,
-            "result": result,
-            "caseId": app.caseCount,
-            "extra": ""
-        });
-        $(".myScore").text(app.totalscore);
+        app.playAnewGame();
+        };
+        console.log(event)
+    },
+    // "click #clickEvent2 img" : function(event){
+    //     if(app.clickStart == false)
+    //         return;
+    //     app.clickStart = false;
+    //     app.caseCount++;
+    //     var element = event.currentTarget;
+    //     var $img = $(event.target);
+    //     var offset = $img.offset();
+    //     var xx = event.clientX - offset.left - 5;
+    //     var yy = event.offsetY-5;
+    //     var x = event.clientX - offset.left;
+    //     var y = event.offsetY;
+    //     x = Math.abs(parseInt(x, 10)) -50;
+    //     y = Math.abs(parseInt(y, 10)) -45;
+    //     x = Math.abs(x);
+    //     y = Math.abs(y);
+    //     $(element).parent().append('<div id="correctdotOnImg" style="position: absolute;top:45%;left:45%;height: 10px;width: 10px;background: green;opacity: 0.6; border-radius: 100%;"></div>');
+    //     $(element).parent().append('<div id="dotOnImg" style="position: absolute;top:'+yy+'px;left:'+xx+'px;height: 10px;width: 10px;background: red;opacity: 0.6; border-radius: 100%;"></div>');
+    //     app.AccuracyPoints = Math.floor( (x + y) / 10 );
+    //     smileDuration = app.randomNumber(parseInt(app.lang.settings.showSmileyMax),parseInt(app.lang.settings.showSmileyMin))
+    //     var str = $(event.currentTarget).attr("src");
+    //     var res = str.match("joy");
+    //     var joySrc = "";
+    //     var rotateFlag = Session.get("rotate");
+    //     if(Session.get("flip") && !rotateFlag){
+    //         var imgsUrl = $("#clickEvent2 div figure.back img");
+    //         for(var i=0,il=imgsUrl.length;i<il;i++){
+    //             var imgSrc = imgsUrl[i].getAttribute("src")
+    //             if(imgSrc.match("joy")){
+    //                 joySrc = imgsUrl[i].src;
+    //                 // console.log("if"+joySrc)
+    //                 imgsUrl[i].src = "./images/expression/smily.png";
+    //                 setTimeout(function(){
+    //                     app.changeFace(joySrc,res);
+    //                 },smileDuration);// correct into smile show s
+    //             }
+    //         }
+    //     }else{
+    //         var imgsUrl = $("#clickEvent2 div figure.front img");
+    //         // console.log(imgsUrl)
+    //         for(var i=0,il=imgsUrl.length;i<il;i++){
+    //             var imgSrc = imgsUrl[i].getAttribute("src")
+    //             if(imgSrc.match("joy")){
+    //                 joySrc = imgsUrl[i].src;
+    //                 // console.log("else"+joySrc)
+    //                 imgsUrl[i].src = "./images/expression/smily.png";
+    //                 setTimeout(function(){
+    //                     app.changeFace(joySrc,res);
+    //                 },smileDuration);// correct into smile
+    //             }
+    //         }
+
+    //     }
+    //     endtime = new Date().getTime()
+    //     totalTime = endtime - app.slideStartTime;
+    //     var delay = app.randomNumber(parseInt(app.lang.settings.tranisionWaitMin),parseInt(app.lang.settings.tranisionWaitMax));
+    //     count--;
+    //     var late = parseInt(app.lang.settings.lateClick);
+    //     if(res){ 
+    //         if(app.score.method){
+    //             if(app.score.method.length!=0){
+    //                 if(totalTime<late){
+    //                     result = parseInt(app.lang.settings.sixteenScorePerHit - app.AccuracyPoints); 
+    //                 }else{
+    //                     result = parseInt(app.lang.settings.sixteenScorePerLateHit);
+    //                 }
+    //             }else{
+    //                     result = parseInt(app.lang.settings.sixteenScorePerHit - app.AccuracyPoints);
+    //             }
+    //         }
+    //         // delay = 2000;
+    //         app.totalscore = app.totalscore + result;
+    //     }else{
+    //         result = 0;
+    //     }
+    //     // console.log(app.AccuracyPoints)
+    //     app.score.method.push({
+    //         "slideStartTime": app.slideStartTime,
+    //         "endtime": endtime,
+    //         "totalTime": totalTime,
+    //         "result": result,
+    //         "caseId": app.caseCount,
+    //         "extra": ""
+    //     });
+    //     $(".myScore").text(app.totalscore);
        
         
-        setTimeout(function(){
-            app.clickStart = true;
-            app.animateFamousRandom();
-            app.chageFlipSession();
-            //
-            // Session.set("esTemplate", "es_surface" +app.getEdgerSwapper())
-        },delay);
-        app.displayProgress(1,app.AccuracyPoints,result)
-        app.selectedImg(event,str)
-    }
+    //     setTimeout(function(){
+    //         app.clickStart = true;
+    //         app.animateFamousRandom();
+    //         app.chageFlipSession();
+    //         //
+    //         // Session.set("esTemplate", "es_surface" +app.getEdgerSwapper())
+    //     },delay);
+    //     app.displayProgress(1,app.AccuracyPoints,result)
+    //     app.selectedImg(event,str)
+    // }
 })
 app.closeCounter2 = function(){
     console.log("startGameLocal")
@@ -211,12 +266,14 @@ app.closeGame2 = function(){
 }
 app.reStartGame2 = function(){   
     game2 = true;
+    app.openOverlay2();
+    // app.startGame2()
     $("#tapTap2").css("display","block");
     $("#tapTapEnded2").css("display","none");
     $(".gametimemins").text("00");
     $(".gametimeseconds").text(":00");  
     $(".myScore").text("0");
-    app.closeCounter();
+    app.closeCounter2();
     gamestart = false;
     app.totalscore = 0;
     seconds=0;
@@ -310,3 +367,15 @@ function endGame2(EndedTime){
     }
 } 
 app.endGame2 = endGame2;
+Template.gameEndPopUp2.events({
+    'click #endGame2': function () {
+        app.reStartGame2();
+    },
+    'click #shareWithFacebook' : function(){
+        app.shareWithFacebook();
+    } 
+});
+app.playAnewGame = function(){
+    $("#rest").css("display","none")
+    $("#famousSquare").css("display","none")
+};
