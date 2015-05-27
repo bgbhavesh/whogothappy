ee417d8cfed0042ebeeb02866cb8f9a59cb8a09d
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-05-27 12:27:26 +0530

    mizzao ntp added

--- server/email/email_test_route.js
+++ server/email/email_test_route.js
@@ -3,3 +3,15 @@ Router.route('/emailTemplate', function () {
   var res = this.response;
   res.end(app.reactEmailTemplate());
 }, {where: 'server'});
+
+Router.route('/emailInvite', function () {
+  var req = this.request;
+  var res = this.response;
+  res.end(app.reactEmailInvite());
+}, {where: 'server'});
+
+Router.route('/emailWeekly', function () {
+  var req = this.request;
+  var res = this.response;
+  res.end(Handlebars.templates['emailInvite']({"email": "nicolsondsouza@gmail.com" ,"username" : "nicolsondsouza","id": "938522329491159" }) );
+}, {where: 'server'});
--- server/email/react_template.js
+++ server/email/react_template.js
@@ -3,21 +3,27 @@ app.reactEmailTemplate = function(){
   // console.log(emailData)
 	return React.renderComponentToString(emailTemplate({"emailData": emailData}));
 }
-
+app.reactEmailInvite = function(){
+  return React.renderComponentToString(emailTemplateInvite({"emailData": null}));
+}
 var emailTemplate = React.createClass({
   render: function() {
     // console.log(this.props.emailData)
     var assImg = [];
     var desImg = [];
     var missImg = [];
-
+    var imgStyle = {
+        "width": "150px",
+        "height": "auto",
+        "font-size": "1rem",
+    }
     var emailData = this.props.emailData;
     for(var i=0,il=emailData.assImg.length;i<il;i++)
-      assImg.push(DOM.img({"src": emailData.assImg[i], key: emailData.assImg[i]}, null));
+      assImg.push(DOM.img({"src": emailData.assImg[i], style: imgStyle}, null));
   	for(var i=0,il=emailData.desImg.length;i<il;i++)
-      desImg.push(DOM.img({"src": emailData.desImg[i], key: emailData.desImg[i]}, null));
+      desImg.push(DOM.img({"src": emailData.desImg[i], style: imgStyle}, null));
     for(var i=0,il=emailData.missImg.length;i<il;i++)
-      missImg.push(DOM.img({"src": emailData.missImg[i], key: emailData.missImg[i]}, null));
+      missImg.push(DOM.img({"src": emailData.missImg[i], style: imgStyle}, null));
     
     return div(null, 
         [
@@ -28,3 +34,49 @@ var emailTemplate = React.createClass({
       );
   },
 });
+
+var emailTemplateInvite = React.createClass({
+  render: function() {
+    // console.log(this.props.emailData)
+    var assImg = [];
+    var desImg = [];
+    var missImg = [];
+    var imgStyle = {
+        "width": "150px",
+        "height": "auto",
+        "font-size": "1rem",
+    }
+    var emailData = this.props.emailData;
+    // for(var i=0,il=emailData.assImg.length;i<il;i++)
+    //   assImg.push(DOM.img({"src": emailData.assImg[i], style: imgStyle}, null));
+    // for(var i=0,il=emailData.desImg.length;i<il;i++)
+    //   desImg.push(DOM.img({"src": emailData.desImg[i], style: imgStyle}, null));
+    // for(var i=0,il=emailData.missImg.length;i<il;i++)
+    //   missImg.push(DOM.img({"src": emailData.missImg[i], style: imgStyle}, null));
+    
+    return div({"style": {"text-align": "center"}}, 
+        [
+          div({"style": {"background-color": "#3498db"}, "width": "100%"}, 
+            [
+              div(null, "Sixteensmiles"),
+              div(null, "See Smiles Everywhere!")
+            ]
+          ),
+          div({"style": {"background-color": "white"}, "width": "100%"}, 
+            [
+              div(null, "Hi, nicolsondsouza@gmail.com"),
+              div(null, 'You have been invited by nicolsondsouza to play "Who got happy?".'),
+              div(null, 'click here to go to the app store. click here to go the application.'),
+            ]
+          ),
+          div({"style": {"background-color": "#3498db"}, "width": "100%"}, 
+            [
+              div(null, "Copyright Â© 2014 SixteenSmiles. All rights reserved."),
+              div(null, "If you do not want to recieve emails from us, you can unsubscribe.")
+            ]
+          ),
+        ]
+      );
+  },
+});
+// #3498db
